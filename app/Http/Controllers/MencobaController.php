d00b5b742794d4ab9ca5944d9a3adb94c772e47d
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-09 20:28:17 +0700

    feat(controllers): add new controller to test new pages (MencobaController.php)

---
+++ app/Http/Controllers/MencobaController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MencobaController extends Controller
+{
+    //Mencoba Controller lain
+    public function boomesport()
+    {
+        return view('boom');
+    }
+    public function prxesport()
+    {
+        return view('prx');
+    }
+    public function fnaticesport()
+    {
+        return view('fnatic');
+    }
+    public function fpxesport()
+    {
+        return view('fpx');
+    }
+    public function beranda()
+    {
+        return view('layout/home');
+    }
+
+}
