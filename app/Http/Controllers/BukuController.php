cf854583d2aab98853eb242ad7f2a2c970cfbbf0
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-17 22:54:06 +0700

    feat(controllers) : add column number for changes id buku into number

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -8,6 +8,7 @@ class BukuController extends Controller
 {
     public function index(){
         $data_buku = Buku::all()->sortByDesc('id');
-        return view('Buku.index', compact('data_buku'));
+        $no = 0;
+        return view('Buku.index', compact('data_buku', 'no'));
     }
 }
