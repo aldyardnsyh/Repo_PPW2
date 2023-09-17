087bf449730f6c9188dc558d9a6decd1321b0829
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-17 22:51:07 +0700

    feat(controllers): add sorting buku descending by id

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -7,8 +7,7 @@ use App\Models\Buku;
 class BukuController extends Controller
 {
     public function index(){
-        $data_buku = Buku::all();
-
+        $data_buku = Buku::all()->sortByDesc('id');
         return view('Buku.index', compact('data_buku'));
     }
 }
