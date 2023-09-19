47e52840e425038c9dde9bf2f68915bdaca32795
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-19 14:46:26 +0700

    feat(views): add new form to add new buku to table database

---
+++ resources/views/buku/create.blade.php
@@ -0,0 +1,12 @@
+<div class="container">
+<h4>Tambah Buku</h4>
+<form action="{{route('buku.store')}}">
+    @csrf
+    <div>Judul <input type="text" name="judul" id="judul" ></div>
+    <div>Penulis <input type="text" name="penulis" id="penulis"></div>
+    <div>Harga <input type="text" name="harga" id="harga"></div>
+    <div>Tanggal Terbit <input type="date" name="tgl_terbit" id="tgl_terbit"></div>
+    <div><button type="submit">Simpan</button></div>
+    <a href="/buku"> Batal</a>
+</form>
+</div>
