22727b96c8479f477aa32399e8d6bd8d0fe7f1a0
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-19 14:45:13 +0700

    feat(controllers): add new function to create and store buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Buku;
+use Illuminate\Http\Request;
 
 class BukuController extends Controller
 {
@@ -12,5 +13,17 @@ class BukuController extends Controller
         $count = Buku::count();
         $total = Buku::sum('harga');
         return view('Buku.index', compact('data_buku', 'no','count','total'));
+    }
+    public function create(){
+        return view('Buku.create');    
+    }
+    public function store(Request $request){
+            $buku = new Buku;
+            $buku->judul = $request->judul;
+            $buku->penulis = $request->penulis;
+            $buku->harga = $request->harga;
+            $buku->tgl_terbit = $request->tgl_terbit;
+            $buku->save();
+            return redirect('/buku');
     }
 }
