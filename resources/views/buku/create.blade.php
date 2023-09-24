21f7c01f06605705865f46ea0d91ffbc77bfb486
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-25 02:45:40 +0700

    feat(views): change new style table for page create

--- resources/views/buku/create.blade.php
+++ resources/views/buku/create.blade.php
@@ -1,12 +1,62 @@
-<div class="container">
-<h4>Tambah Buku</h4>
-<form action="{{route('buku.store')}}">
-    @csrf
-    <div>Judul <input type="text" name="judul" id="judul" ></div>
-    <div>Penulis <input type="text" name="penulis" id="penulis"></div>
-    <div>Harga <input type="text" name="harga" id="harga"></div>
-    <div>Tanggal Terbit <input type="date" name="tgl_terbit" id="tgl_terbit"></div>
-    <div><button type="submit">Simpan</button></div>
-    <a href="/buku"> Batal</a>
-</form>
-</div>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tambah Buku</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
+    <style>
+        /* Gaya untuk header dengan warna yang sama dengan tombol tambah */
+        .card-header {
+            background-color: #17a2b8;
+            color: #fff;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <div class="container mt-4">
+        <div class="row justify-content-center">
+            <div class="col-md-6">
+                <div class="card">
+                    <!-- Header dengan warna -->
+                    <div class="card-header">
+                        <h4>Tambah Buku</i></h4>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{ route('buku.store') }}" method="POST">
+                            @csrf
+                            <div class="form-group">
+                                <label for="judul">Judul</label>
+                                <input type="text" class="form-control" id="judul" name="judul" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="penulis">Penulis</label>
+                                <input type="text" class="form-control" id="penulis" name="penulis" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="harga">Harga</label>
+                                <input type="text" class="form-control" id="harga" name="harga" required>
+                            </div>
+                            <div class="form-group">
+                                <label for="tgl_terbit">Tanggal Terbit</label>
+                                <input type="date" class="form-control" id="tgl_terbit" name="tgl_terbit" required>
+                            </div>
+                            <div class="text-center">
+                                <button type="submit" class="btn btn-primary"><i class="fas fa-save"></i> Simpan</button>
+                                <a href="/buku" class="btn btn-secondary ml-2">Batal</a>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!-- Tambahkan link ke Bootstrap JS dan jQuery jika Anda menggunakan Bootstrap -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+</body>
+</html>
