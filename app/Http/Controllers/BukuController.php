6f5683c56d8497255863d3fc7e3ca3483e1c0448
aldyardnsyh <aldyardiansyah@ugm.ac.id>
2023-09-25 02:46:42 +0700

    feat(controllers): add new function for edit and update buku

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -32,4 +32,20 @@ class BukuController extends Controller
         $buku->delete();
         return redirect('/buku');
     }
+
+    public function edit($id){
+        $buku = Buku::find($id);
+        return view('Buku.edit', compact('buku'));
+
+    }
+    public function update(Request $request, $id){
+        $buku = Buku::find($id);
+        $buku->judul = $request->judul;
+        $buku->penulis = $request->penulis;
+        $buku->harga = $request->harga;
+        $buku->tgl_terbit = $request->tgl_terbit;
+        $buku->save();
+        return redirect('/buku');
+    }
+
 }
