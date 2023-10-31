816ba5cf28e4127e909b9b84921771744e6847c1
aldyardnsyh <aldyardiansyah@mail.ugm.ac.id>
2023-10-31 22:13:34 +0700

    feat(routes):  change the configuration of the new routes

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,7 @@
 <?php
 
 use App\Http\Controllers\BukuController;
-use App\Http\Controllers\MencobaController;
-use App\Models\Buku;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,35 +15,58 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+// Route::get('/', function () {
+//     return view('auth/login');
+// });
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
+
+// // Menyediakan rute login yang tetap ada di /login
+// Route::get('/login', function () {
+//     return view('auth/login');
+// })->name('login');
+
+// require __DIR__.'/auth.php';
+
+// Route::get('/dashboard', function () {
+//     return view('dashboard');
+// })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about', [
-        "name" => "pragos",
-        "email" => "pragos@gmail.com"
-    ]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/buku', [BukuController::class, 'index'])->name('buku.index');
+    Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
+    Route::post('/buku', [BukuController::class, 'store'])->name('buku.store');
+    Route::delete('/buku/delete/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
+
+    // Edit Buku
+    Route::get('/buku/edit/{id}', [BukuController::class, 'edit'])->name('buku.edit');
+
+    // Update Buku
+    Route::post('/buku/update/{id}', [BukuController::class, 'update'])->name('buku.update');
+
+    // Cari Buku
+    Route::get('/buku/search', [BukuController::class, 'search'])->name('buku.search');
 });
 
-Route::get('/boom',[MencobaController::class,'boomesport']);
-
-Route::get('/prx',[MencobaController::class,'prxesport']);
-
-Route::get('/fnatic',[MencobaController::class,'fnaticsport']);
-
-Route::get('/fpx',[MencobaController::class,'fpxesport']);
-
-Route::get('/',[MencobaController::class,'beranda']);
-
-Route::get('/buku', [BukuController::class,'index']);
-Route::get('/buku/create', [BukuController::class, 'create'])->name('buku.create');
-Route::post('/buku/store', [BukuController::class, 'store'])->name('buku.store');
-// Buku Delete
-Route::delete('/buku/{id}', [BukuController::class, 'destroy'])->name('buku.destroy');
-// Buku Edit
-Route::get('/buku/edit/{id}', [BukuController::class, 'edit'])->name('buku.edit');
-// Buku Update
-Route::post('/buku/update/{id}', [BukuController::class, 'update'])->name('buku.update');
-// Fitur Search
-Route::get('/buku/search', [BukuController::class, 'search'])->name('buku.search');
+require __DIR__.'/auth.php';
