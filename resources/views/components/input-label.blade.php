a72daf4205240374cc07b12a590fb66f987a4513
aldyardnsyh <aldyardiansyah@mail.ugm.ac.id>
2023-11-14 10:22:38 +0700

    feat: refactor input label to prevent XSS vulnerability

--- resources/views/components/input-label.blade.php
+++ resources/views/components/input-label.blade.php
@@ -1,5 +1,5 @@
 @props(['value'])
 
 <label {{ $attributes->merge(['class' => 'block font-medium text-sm text-gray-700']) }}>
-    {{ $value ?? $slot }}
+    {{ is_array($value) ? '' : htmlspecialchars($value, ENT_QUOTES, 'UTF-8') }}
 </label>
