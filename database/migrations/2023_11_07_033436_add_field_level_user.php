4391cd3a364a18df70f699e76eeb8ec9c3adad9b
aldyardnsyh <aldyardiansyah@mail.ugm.ac.id>
2023-12-04 01:16:41 +0700

    chore(database): fix table name in migration and add enum field for user level

--- database/migrations/2023_11_07_033436_add_field_level_user.php
+++ database/migrations/2023_11_07_033436_add_field_level_user.php
@@ -9,7 +9,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('level')->default('user');
+            $table->enum('level', ['admin', 'user'])->default('user');
         });
     }
 
